c191376668962da1928b42bf2eb14565cd849496
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-01-27 10:56:04 +0900

    init: C++ basic

---
+++ define.cpp
@@ -0,0 +1,15 @@
+// define : 상수, 매크로를 정의할 수 있다.
+#include<bits/stdc++.h>
+using namespace std;
+#define PI 3.14159 
+#define loop(x, n) for(int x = 0; x < n; x++)
+
+int main() {
+    cout << PI << '\n';
+    int sum = 0;
+    loop(i, 10) {
+        sum += i;
+    }
+    cout << sum << '\n';
+    return 0;
+}
---
+++ typedef.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef int i; // typedef : 타입의 이름을 새로이 별칭으로 정의하고, 실제 타입 이름 대신 별칭으로 사용할 수 있도록 한다.
+i main() {
+    i a = 1;
+    cout << a << "\n";
+    return 0;
+}
