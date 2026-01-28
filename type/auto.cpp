32e2a3f8e3e82e414084f4a916fc0fb24854f35d
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-01-28 16:45:00 +0900

    add: auto

---
+++ type/auto.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a = 1;
+auto b = 1;
+int main() {
+    cout << b << "\n";
+    return 0;
+}
+
+/*
+1
+
+auto : 타입추론을 하여 결정되는 타입이다.
+auto 타입은 주로 복잡하고 긴 타입의 변수명을 대신할 떄 사용됩니다.
+*/
