71fb4d69ff168d969778609dcb1ab137fa5e4ab8
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-01-28 15:39:49 +0900

    comment: 주석 추가

--- type/for_loop.cpp
+++ type/for_loop.cpp
@@ -7,5 +7,18 @@ int main(){
     for (int i = 0; i < a.size(); i++) {
         cout << a[i] << "\n";
     }
-    return 0
+    return 0;
 }
+/*
+1
+2
+3
+1
+2
+3 
+
+C++ 11부터 범위기반 for 루프가 추가되어 이를 사용할 수 있다.
+for ( range_declaration : range_expression ) loop_statement
+for({타입} {임시변수명} : {타입을 담은 컨테이너})
+
+*/
