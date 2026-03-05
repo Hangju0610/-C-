0e33e8c76ba21a5b624a433aba8dd3ff258b3aec
Hangju0610 <131460159+Hangju0610@users.noreply.github.com>
2026-03-05 11:21:12 +0900

    add: pow, sqrt

---
+++ math/pow.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 4;
+    int pow_2 = (int)pow(2, n);
+    cout << pow_2 << '\n';
+    return 0;
+}
+
+/*
+pow(double base, double exponent);
+
+기본적으로 double을 반환하기 때문에 int 형을 사용하고 싶다면 int로 형변환을 한다.
+*/
---
+++ math/sqrt.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n = 16;
+    int ret = (int)sqrt(n);
+    cout << ret << '\n';
+    return 0;
+}
+
+/*
+sqrt(double num);
+double을 return 하기 때문에 타입캐스팅 필수.
+*/
